c1b145478fc50bd87b759807abe7bdd19348f587
Litha22 <lithanelani1@gmail.com>
2018-12-07 09:00:25 +0200

    Updated Fonts and background images

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
-$(document).ready(function() {
-    var aparece = function() {
-      $('.fadein').each(function(i) {
-        var bottom_of_object = $(this).position().top + $(this).outerHeight();
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
-        /* Se o objecto está totalmente dentro do screen... faz fade */
-        if (bottom_of_window > bottom_of_object) {
-          $(this).animate({ 'opacity': '1', 'padding-top': '180px' }, 1000, "easeInOutExpo");
+
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 50) {
+          $(".grid-container1").css("background" , "$primary-color");
         }
-      });
-    }
-    aparece();
-    $(window).scroll(function() {
-      aparece();
-    });
+  
+        else{
+            $(".grid-container1").css("background" , "transparent");  	
+        }
+    })
   })
